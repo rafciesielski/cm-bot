201e61f5ec31cff950ba25a94898d22e2cb70710
rciesielski <rciesielski@consol.pl>
2017-10-12 18:27:41 +0200

    1

--- server.js
+++ server.js
@@ -113,6 +113,28 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
   }
 })
 
+
+slapp.message('yesno', (msg) => {
+  msg.say({
+      text: '',
+      attachments: [
+        {
+          text: '',
+          fallback: 'Yes or No?',
+          callback_id: 'yesno_callback',
+          actions: [
+            { name: 'answer', text: 'Yes', type: 'button', value: 'yes' },
+            { name: 'answer', text: 'No',  type: 'button',  value: 'no' }
+          ]
+        }]
+      })
+})
+
+slapp.action('yesno_callback', 'answer', (msg, value) => {
+  msg.respond(msg.body.response_url, `${value} is a good choice!`)
+})
+
+
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
 
