a9a06814199ad916c04580be37a64a8640d4966f
rciesielski <rciesielski@consol.pl>
2017-10-13 09:28:42 +0200

    1

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ var queue_options = {
 
 slapp.command('newticket', (msg) => {
     var subject = (msg.body.event && msg.body.event.text)
-    console.log('New ticket: ' + msg.body.event.text);
+    //console.log('New ticket: ' + msg.body.event.text);
+    msg.say('qqqqqqqqqq')
 })
 
 
