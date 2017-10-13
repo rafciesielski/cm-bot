704094888a277a417fed2925cf295855f76d9c58
rciesielski <rciesielski@consol.pl>
2017-10-13 10:16:29 +0200

    1

--- server.js
+++ server.js
@@ -68,11 +68,31 @@ slapp.action('priority_callback', 'answer', (msg, value) => {
     msg.respond(msg.body.response_url, `Priority: ${value}`)
 })
 
+var country_options = {
+    text: '',
+    attachments: [
+        {
+            text: 'Select country:',
+            fallback: 'Select country:',
+            callback_id: 'country_callback',
+            actions: [
+                { name: 'answer', text: 'Germany', type: 'button', value: 'germany' },
+                { name: 'answer', text: 'France', type: 'button', value: 'france' },
+                { name: 'answer', text: 'Poland', type: 'button', value: 'poland' }
+            ]
+        }]
+}
+
+slapp.action('country_callback', 'answer', (msg, value) => {
+    msg.respond(msg.body.response_url, `Country: ${value}`)
+})
+
 slapp.command('/newticket', (msg, subject) => {
     console.log('New ticket: ' + subject);
     msg.say(`Subject: ${subject}`)
         .say(queue_options)
         .say(priority_options)
+        .say(country_options)
 })
 
 // Catch-all for any other responses not handled above
